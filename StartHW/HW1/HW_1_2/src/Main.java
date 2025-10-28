23f01cab8473a505e337650140413ae18b0558bb
NGanzyuk <nganzyuk.edu@gmail.com>
2025-10-28 16:46:10 +0300

    fix

---
+++ StartHW/HW1/HW_1_2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    static void main(String[] args) {
+        int income = 44000, spending = 30000;
+        System.out.println("\nИтого:\n" + (income - spending) + " руб.");
+    }
+}
--- StartHW/HW1/src/Main.java
+++
@@ -1,7 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    System.out.println("Ganzyuk Nikita");
-    int income = 44000, spending = 30000;
-    System.out.println("\nИтого:\n" + (income - spending) + " руб.");
-}
